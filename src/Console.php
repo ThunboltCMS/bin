3f371b0cddf6eb7d0ff9402322d92aa7642a5c92
MartkCz <antik494@gmail.com>
2017-05-25 11:50:59 +0200

    php 7.1

--- src/Console.php
+++ src/Console.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Thunbolt\Console;
 
 class Console {
 
-	public static function chooseBool() {
+	public static function chooseBool(): bool {
 		echo 'Your choose (y/n): ';
 		$stdin = fopen('php://stdin', 'r');
 		$response = strtolower(fgetc($stdin));
@@ -17,28 +19,28 @@ class Console {
 		return $response === 'y';
 	}
 
-	public static function error($msg) {
+	public static function error(string $msg): void {
 		fprintf(STDERR, "\033[31m" . $msg . "\033[39m\n");
 		exit(1);
 	}
 
-	public static function warning($msg) {
+	public static function warning(string $msg): void {
 		fprintf(STDOUT, "\033[33m" . $msg . "\033[39m\n");
 	}
 
-	public static function info($msg) {
+	public static function info(string $msg): void {
 		fprintf(STDOUT, $msg . "\n");
 	}
 
-	public static function bold($msg) {
+	public static function bold(string $msg): void {
 		return "\033[1m" . $msg . "\033[22m";
 	}
 
-	public static function command($msg) {
+	public static function command(string $msg): void {
 		fprintf(STDOUT, "\033[35m" . self::bold($msg) . "\033[39m\n");
 	}
 
-	public static function existScript($name) {
+	public static function existScript(string $name): bool {
 		exec('command -v ' . $name, $x, $return);
 
 		return $return === 0;
